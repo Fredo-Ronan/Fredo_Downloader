ff13a9dfcd048e8c231f72cea59001b3a1ce884d
Fredo-Ronan <fredocode06@gmail.com>
2024-02-29 21:16:00 +0700

    set version constant to``1.0.0`` and api link

--- constants/constants.py
+++ constants/constants.py
@@ -2,10 +2,10 @@
     This file contains some constant values for the app
 """
 
-APP_VERSION = "1.2.5"
+APP_VERSION = "1.0.0"
 APP_NAME = "Fredo Downloader"
 DEVELOPER = "Fredo Ronan"
 WEB_PORTO = "https://fredo-ronan.github.io/"
 
-FETCH_VERSION_API_LINK = "https://api.github.com/repos/Fredo-Ronan/Android-IG-Downloader-using-Python/releases/latest"
-LATEST_VER_LINK = "https://github.com/Fredo-Ronan/Android-IG-Downloader-using-Python/releases/latest"
+FETCH_VERSION_API_LINK = "https://api.github.com/repos/Fredo-Ronan/Fredo_Downloader/releases/latest"
+LATEST_VER_LINK = "https://github.com/Fredo-Ronan/Fredo_Downloader/releases/latest"
